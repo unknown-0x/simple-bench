36494b3e7b6dbe09dc1df5053853f12ac99c6295
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-04-08 21:53:28 +0700

    Add new macros

--- src/simplebench/simplebench_macros.hpp
+++ src/simplebench/simplebench_macros.hpp
@@ -19,4 +19,35 @@ SIMPLEBENCH_API Benchmark& RegisterBenchmark(const char* name,
           ::simplebench::Benchmark::Config(__VA_ARGS__)); \
   void BenchmarkFunc##name()
 
+#define BENCHMARK_PARAMS(name, ...) void BenchmarkFunc##name(__VA_ARGS__)
+
+#define BENCHMARK_ARGS(name, secondary_name, ...)       \
+  static ::simplebench::Benchmark::Config&              \
+      _BenchmarkConfig_##name##secondary_name## =       \
+          ::simplebench::internal::RegisterBenchmark(   \
+              #name "/" #secondary_name,                \
+              [] { BenchmarkFunc##name(__VA_ARGS__); }, \
+              ::simplebench::Benchmark::Config())       \
+              .GetConfig()
+
+#define BENCHMARK_TEMPLATE(name) void BenchmarkTemplate##name()
+
+#if defined(__COUNTER__)
+#define SIMPLEBENCH_UNIQUE_ID __COUNTER__
+#else
+#define SIMPLEBENCH_UNIQUE_ID __LINE__
+#endif
+
+#define SIMPLEBENCH_CONCAT_IMPL(x, y) x##y
+#define SIMPLEBENCH_CONCAT(x, y) SIMPLEBENCH_CONCAT_IMPL(x, y)
+
+#define BENCHMARK_TEMPLATE_REGISTER(name, ...)                 \
+  static ::simplebench::Benchmark::Config& SIMPLEBENCH_CONCAT( \
+      _BenchmarkConfig_##name, SIMPLEBENCH_UNIQUE_ID) =        \
+      ::simplebench::internal::RegisterBenchmark(              \
+          #name "<" #__VA_ARGS__ ">",                          \
+          [] { BenchmarkTemplate##name<__VA_ARGS__>(); },      \
+          ::simplebench::Benchmark::Config())                  \
+          .GetConfig()
+
 #endif
