435575d98e6cdec546a6020bc9e0036d184840c7
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-04-26 22:01:31 +0700

    Update

--- benchmark/basic_example.cpp
+++ benchmark/basic_example.cpp
@@ -4,20 +4,18 @@
 
 const double pi = std::acos(-1);
 
-BENCHMARK(Sin_v1) {
-  const auto s1 = std::sin(pi / 6);
-  const auto s2 = std::sin(pi / 2);
-  const auto s3 = std::sin(-3 * pi / 4);
+void compute_sin() {
+  const auto s1 = std::sin(pi / 6.0);
+  const auto s2 = std::sin(pi / 2.0);
+  const auto s3 = std::sin(-3.0 * pi / 4.0);
 }
 
-BENCHMARK(Sin_v2) {
-  const auto s1 = std::sin(pi / 6);
-  const auto s2 = std::sin(pi / 2);
-  const auto s3 = std::sin(-3 * pi / 4);
-}
+int main(int argc, char** argv) {
+  simplebench::Initialize(argc, argv);
 
-BENCHMARK(Sin_v3) {
-  const auto s1 = std::sin(pi / 6);
-  const auto s2 = std::sin(pi / 2);
-  const auto s3 = std::sin(-3 * pi / 4);
+  simplebench::RegisterBenchmark("sin() v1", compute_sin);
+  simplebench::RegisterBenchmark("sin() v2", compute_sin);
+  simplebench::RegisterBenchmark("sin() v3", compute_sin);
+
+  return simplebench::RunAllBenchmarks();
 }
